07d70d733cab4a64c5f30ca9993e3d505a303b3e
Michael Joy <kodiakchaos@gmail.com>
2023-09-09 22:11:53 -0400

    should use ExpenseCategory

--- src/services/Database.ts
+++ src/services/Database.ts
@@ -171,7 +171,7 @@ class Database extends Dexie {
   async getExpensesForMonthYearCategory(
     month: number,
     year: number,
-    category: string
+    category: ExpenseCategory
   ): Promise<Expense[]> {
     const start = new Date(year, month, 1)
     const end = new Date(year, month + 1, 0, 23, 59, 59, 999)
